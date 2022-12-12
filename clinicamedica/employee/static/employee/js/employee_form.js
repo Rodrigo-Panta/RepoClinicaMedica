caa1eac8e6526df20f2e977a6186ca89706ccf2b
Rodrigo Luiz Panta Junior <rodrigo.panta@dtidigital.com.br>
2022-12-11 22:04:05 -0300

    auto fill address data on employee create

--- clinicamedica/employee/static/employee/js/employee_form.js
+++ clinicamedica/employee/static/employee/js/employee_form.js
@@ -1,9 +1,12 @@
 document.addEventListener("DOMContentLoaded", function(e) { 
-    document.getElementById('id_is_doctor').addEventListener('change', toggleDoctorForm);
-    toggleDoctorForm();
-  });
+    document.getElementById('id_is_doctor').addEventListener('change', updateDoctorForm);
+    updateDoctorForm();
 
-function toggleDoctorForm(){
+    document.getElementById('id_cep').addEventListener('keyup', searchAddress);
+
+});
+
+function updateDoctorForm(event){
     let checkbox = document.getElementById('id_is_doctor');
     let crm_input = document.getElementById("id_crm");
     let specialty_input = document.getElementById("id_specialty");
@@ -19,6 +22,37 @@ function toggleDoctorForm(){
         crm_input.required = false;
         specialty_input.required = false;
     }
-    console.log(value);
 }
 
+function searchAddress(event){
+    let value = document.getElementById('id_cep').value;
+    
+    if(value.length == 8){
+        setTimeout(() => {
+            let url = document.getElementById('api_get_address_url').value + value;
+            httpGetAsync(url, updateAddress);
+        }, "500")
+
+    }
+
+}
+
+function updateAddress(address_str){
+    if(address_str){
+        let address = JSON.parse(address_str);
+        for(key in address){
+            document.getElementById(`id_${key}`).value = address[key];  
+        }
+    }
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
