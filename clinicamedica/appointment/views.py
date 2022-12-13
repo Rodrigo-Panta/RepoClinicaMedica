1e1b0007287fbfac980aea74f10275b04cd734f0
gedeaopereira <82479616+gedeaopereira@users.noreply.github.com>
2022-12-13 17:38:19 -0300

    Lista de Agendamentos

--- clinicamedica/appointment/views.py
+++ clinicamedica/appointment/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 
 from .models import Appointment
 from .forms import AppointmentForm
@@ -15,7 +17,12 @@ from utils.time_choices import time_choices
 class AppointmentCreateView(CreateView):
     model = Appointment
     template_name = 'appointment/appointment_form.html'    
-    form_class = AppointmentForm
+    fields = '__all__'
+    
+class AppointmentListView(ListView, LoginRequiredMixin):
+    model = Appointment
+    template_name = 'appointment/list.html'    
+    fields = '__all__'    
 
 
 def api_get_available_times(request, doctor_id, date):
