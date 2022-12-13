0958cce3bf7bb7e5ed28bbc225b1d12416d82f33
gedeaopereira <82479616+gedeaopereira@users.noreply.github.com>
2022-12-13 17:21:32 -0300

    Favicon

--- clinicamedica/address/urls.py
+++ clinicamedica/address/urls.py
@@ -5,6 +5,7 @@ app_name='address'
 
 urlpatterns = [
     path('create/', views.AddressCreateView.as_view(), name='create_address'),
+    path('list/', views.AddressListView.as_view(), name='list_address'),
     path('api/get/<cep>', views.get_address, name='api_get_address'),
     path('api/get', views.get_address, name='api_get_address'),
 ]
