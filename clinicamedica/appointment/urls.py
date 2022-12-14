2971c60f14344a311d79a16f6b3affd75d491acb
gedeaopereira <82479616+gedeaopereira@users.noreply.github.com>
2022-12-13 22:28:30 -0300

    ok

--- clinicamedica/appointment/urls.py
+++ clinicamedica/appointment/urls.py
@@ -4,9 +4,9 @@ from . import views
 app_name='appointment'
 
 urlpatterns = [
-    path('', views.AppointmentListView.as_view(), name='list_appointment'),
     path('create/', views.AppointmentCreateView.as_view(), name='create_appointment'),
     path('list/', views.AppointmentListView.as_view(), name='list_appointment'),
+    path('', views.AppointmentListView.as_view(), name='list_appointment'),
     path('api/get_available_times', views.api_get_available_times, name='api_get_available_times'),
     path('api/get_available_times/<doctor_id>/<date>', views.api_get_available_times, name='api_get_available_times'),
 ]
